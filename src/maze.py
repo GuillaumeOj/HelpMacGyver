5acbb56107b7d0942b2da840ef05046eca0998fe
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2019-08-30 14:35:33 +0200

    Modify the level reading and create textures for the maze

--- src/maze.py
+++ src/maze.py
@@ -7,7 +7,10 @@
     It contain the classe 'Maze'
 """
 import sys
-import os
+
+import pygame
+
+from main import CELL_WIDTH, CELL_HEIGHT
 
 
 class Maze:
@@ -18,24 +21,60 @@ class Maze:
             - structure
     """
 
-    def __init__(self, level, background_src):
+    def __init__(self, level, backgrounds):
         """
             Read the level's file
             Create the maze structure
             Create the maze background
         """
-        self.background_src = background_src
-
+        # Load the 'backgrounds' ressource
         try:
-            with open(os.path.join(os.getcwd(), level), mode='r') as file:
+            backgrounds = pygame.image.load(backgrounds)
+        except pygame.error:
+            # If someone move or delete the file
+            print(f'{backgrounds} was not found')
+            sys.exit()
+
+        # Define texture for the walls
+        wall = pygame.Surface((20, 20))
+        wall.blit(backgrounds, (-20 * 9, 0))
+        wall = self.transform_scale(wall, CELL_WIDTH, CELL_HEIGHT)
+
+        # Define texture for the floor
+        floor = pygame.Surface((20, 20))
+        floor.blit(backgrounds, (0, -20 * 4))
+        floor = self.transform_scale(floor, CELL_WIDTH, CELL_HEIGHT)
+
+        # Define texture for the start
+        start = pygame.Surface((20, 20))
+        start.blit(backgrounds, (-20 * 4, -20 * 5))
+        start = self.transform_scale(start, CELL_WIDTH, CELL_HEIGHT)
+
+        # Define texture for the end
+        end = pygame.Surface((20, 20))
+        end.blit(backgrounds, (-20 * 4, -20 * 5))
+        end = self.transform_scale(end, CELL_WIDTH, CELL_HEIGHT)
+
+        # Load the level '.txt' file and store it in a double list
+        try:
+            with open(level, mode='r') as file:
                 # Read the file
                 data = file.read()
 
                 # Store the maze in a double list
                 self.maze = [list(row) for row in data.split('\n')]
         except FileNotFoundError:
-            print(f'{os.path.join(os.getcwd(), level)} was not found')
+            # If someone move or delete the file
+            print(f'{level} was not found')
             sys.exit()
+
+    @staticmethod
+    def transform_scale(surface, width, height):
+        """
+            Just transform with 'scale' a 'surface'
+            to a new 'width' and a new 'height'
+        """
+        return pygame.transform.scale(surface, (width, height))
 
 if __name__ == '__main__':
     print('Error, not the main file.')
