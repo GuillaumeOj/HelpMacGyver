8c17f712bff7d91e31948040adeec0e0d3a37cfc
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2019-08-30 15:24:53 +0200

    Update '__init__.py' for easy use in 'main.py'

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    Author: GuillaumeOj
+    Version: 0.0.1
+
+    Setup imports for main
+"""
+from .maze import Maze
+from .maze_config import CELL_WIDTH, CELL_HEIGHT, MAZE_WIDTH, MAZE_HEIGHT, PANEL_WIDTH
