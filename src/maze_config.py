fcdb059ee4a64fea6132a1ec151680913d42b45a
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2019-09-03 15:33:23 +0200

    Add a title to the 'Panel' and a 'stuff' area (to stock picked up items)

--- src/maze_config.py
+++ src/maze_config.py
@@ -6,11 +6,20 @@
     Configuration file for the maze constants
 """
 
+# Configure the maze's cells dimensions
 CELL_WIDTH = 30
 CELL_HEIGHT = 30
+
+# Configure the maze's rows and column number
 MAZE_WIDTH = 15
 MAZE_HEIGHT = 15
+
+# Configure the right panel width
 PANEL_WIDTH = 200
+
+# Configure the stuff size
+STUFF_ROW = 2
+STUFF_COLUMN = 3
 
 if __name__ == '__main__':
     print('Error, not the main file.')
--- src/panel.py
+++ src/panel.py
@@ -6,17 +6,22 @@
     Panel module for the McGyver game
     It contain the class 'Panel' for manage the panel on the right side
 """
+import sys
+import os
 
 import pygame
 
 # pylint: disable=wildcard-import
 from .maze_config import *
 
-
+# pylint: disable=too-few-public-methods
 class Panel:
     """
         Configure and manage the right panel on th screen
     """
+
+    # List of slots
+    slots = list()
 
     def __init__(self):
         """
@@ -34,6 +39,69 @@ class Panel:
         pygame.draw.rect(self.background, (100, 47, 35), border_rect, 10)
 
         self.position = (CELL_WIDTH * MAZE_WIDTH, 0)
+
+        self._create_font('Stuffs', 20, (border_rect.width / 2, 20), True)
+
+        # Create a surface to stock all picked up items
+        residue = (PANEL_WIDTH - STUFF_COLUMN * CELL_WIDTH) / (STUFF_COLUMN + 1)
+        self.stuff = pygame.Surface((STUFF_COLUMN * CELL_WIDTH + residue * (STUFF_COLUMN - 1),
+                                     STUFF_ROW * CELL_HEIGHT + residue * (STUFF_ROW - 1)))
+        self.stuff.fill((159, 112, 76))
+
+        # Create each slot
+        for row in range(STUFF_ROW):
+            for column in range(STUFF_COLUMN):
+                self._stuff_slot((column * (CELL_WIDTH + residue), (row * (CELL_HEIGHT + residue))))
+
+        self.background.blit(self.stuff, (residue, 50))
+
+    def _stuff_slot(self, position):
+        """
+            This method create stuff slot for a  given 'position'
+        """
+
+        # Create a surface for the slot
+        slot = pygame.Surface((CELL_WIDTH, CELL_HEIGHT))
+        slot.fill((63, 45, 42))
+
+        # Store the slot in a list
+        Panel.slots.append(slot)
+
+        # Blit the 'slot' with the 'background'
+        self.stuff.blit(slot, position)
+
+
+    def _create_font(self, text, size, position, center=False):
+        """
+            This function create a text:
+                - Font type
+                - Font size
+                - Text
+                - Position
+        """
+        pygame.font.init()
+
+        # Find the font in 'ressources/'
+        font_src = os.path.join('ressources', 'ka1.ttf')
+
+        # Load the font
+        try:
+            font = pygame.font.Font(font_src, size)
+        except FileNotFoundError:
+            # If someone move or delete the file
+            print(f'{font_src} was not found')
+            sys.exit()
+
+        # Render the font
+        font = font.render(text, True, (100, 47, 35))
+        if center:
+            font_rect = font.get_rect(center=position)
+        else:
+            font_rect = font.get_rect(position)
+
+        self.background.blit(font, font_rect)
+
+        pygame.font.quit()
 
 if __name__ == '__main__':
     print('Error, not the main file.')
